c6865add5c9877ec4f7faefbc78cf0ca728dd839
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-11-03 15:47:51 -0600

    created auth-routes functionality

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,8 +3,29 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+// TODO: If the user exists and the password is correct, return a JWT token
 export const login = async (req: Request, res: Response) => {
-  // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ 
+    where: { username },
+  });
+
+  if(!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if(!validPassword) {
+    return res.status(401).json({ message: 'Invalid password' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+  return res.json({ token });
+
 };
 
 const router = Router();
